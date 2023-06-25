20eaf058df9d453456f6e3dbec6dcfbac6b1bc6a
Vladimir Shytsko <shytsko.v@gmail.com>
2023-06-25 08:32:06 +0300

    Add home page

---
+++ homework1/database.py
@@ -0,0 +1,15 @@
+categories_data = [
+    {'cat_title': 'Одежда',
+     'cat_link': 'clothing',
+     'cat_description': 'With supporting text below as a natural lead-in to additional content.'},
+    {'cat_title': 'Обувь',
+     'cat_link': 'shoes',
+     'cat_description': 'With supporting text below as a natural lead-in to additional content.'},
+    {'cat_title': 'Куртки',
+     'cat_link': 'jackets',
+     'cat_description': 'With supporting text below as a natural lead-in to additional content.'}
+]
+
+
+def get_categories():
+    return categories_data
