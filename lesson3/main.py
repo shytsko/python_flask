8fae22e30fbeb24d826c715bae1c5d14e43366af
Vladimir Shytsko <shytsko.v@gmail.com>
2023-06-29 01:15:55 +0300

    Add lesson 3

---
+++ lesson3/main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
+from models import db, User, Post, Comment
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+db.init_app(app)
+
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+    print('OK')
+
+
+@app.cli.command("add-john")
+def add_user():
+    user = User(username='john', email='john@example.com')
+    db.session.add(user)
+    db.session.commit()
+    print('John add in DB!')
+
+
+@app.cli.command("edit-john")
+def edit_user():
+    user = User.query.filter_by(username='john').first()
+    user.email = 'new_email@example.com'
+    db.session.commit()
+    print('Edit John mail in DB!')
+
+
+@app.cli.command("del-john")
+def del_user():
+    user = User.query.filter_by(username='john').first()
+    db.session.delete(user)
+    db.session.commit()
+    print('Delete John from DB!')
+
+
+@app.route('/')
+def text():
+    return 'Hi'
+
+
+if __name__ == '__main__':
+    app.run()
