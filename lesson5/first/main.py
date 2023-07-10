36f47019928d09e050505e4a7814a3033766f2de
Vladimir Shytsko <shytsko.v@gmail.com>
2023-07-10 14:02:54 +0300

    Update lesson5

--- lesson5/first/main.py
+++ lesson5/first/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+import logging
+from typing import Optional
+from pydantic import BaseModel
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 
 @app.get('/')
@@ -8,6 +21,25 @@ async def root():
     return {'msg': 'Hello World!'}
 
 
-@app.get('/items/{id}')
-async def get_items(id: int, q: int = None):
-    return {'id': id, 'q': q}
+@app.get("/items/")
+async def get_item(item_id: int):
+    logger.info('Отработал GET запрос.')
+    return {"item_id": item_id}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    logger.info('Отработал POST запрос.')
+    return item
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    logger.info(f'Отработал PUT запрос для item id = {item_id}.')
+    return {"item_id": item_id, "item": item}
+
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    logger.info(f'Отработал DELETE запрос для item id ={item_id}.')
+    return {"item_id": item_id}
